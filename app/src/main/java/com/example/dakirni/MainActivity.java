64be48d30878f15f99a53533fcd1e44a622af729
ismail-ERK <82320812+ismail-ERK@users.noreply.github.com>
2021-12-16 23:22:32 +0100

    father & son interfaces commit

--- app/src/main/java/com/example/dakirni/MainActivity.java
+++ app/src/main/java/com/example/dakirni/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.dakirni;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Father.class);
+                startActivity(intent);
+            }
+        });
     }
 }
