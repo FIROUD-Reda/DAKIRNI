5dbf45faaa7d4713d5fe329077ffd92b6a6acc84
kaoutar oubenaddi <kaoutar.oubenaddi@gmail.com>
2022-01-02 11:26:12 +0100

    [OUBENADDI KAOUTAR] map

--- app/src/main/java/com/example/dakirni/ui/safezone/SafezoneFragment.java
+++ app/src/main/java/com/example/dakirni/ui/safezone/SafezoneFragment.java
@@ -4,58 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.example.dakirni.R;
-import com.example.dakirni.databinding.ActivitySafeZoneBinding;
-import com.example.dakirni.databinding.FragmentSafezoneBinding;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SafezoneFragment extends FragmentActivity implements OnMapReadyCallback {
+public class SafezoneFragment extends Fragment implements OnMapReadyCallback {
 
-    private SafezoneViewModel safezoneViewModel;
-    private FragmentSafezoneBinding binding;
-
-    private GoogleMap mMap;
-
-    public View onCreate(@NonNull LayoutInflater inflater,
+    public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        safezoneViewModel =
-                new ViewModelProvider(this).get(SafezoneViewModel.class);
-
-        binding = FragmentSafezoneBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
+        View root = inflater.inflate(R.layout.fragment_safezone, container, false);
+        SupportMapFragment supportMapFragment;
+        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (supportMapFragment == null) {
+            FragmentManager fragmentManager = getFragmentManager();
+            if(fragmentManager != null) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                supportMapFragment = SupportMapFragment.newInstance();
+                fragmentTransaction.replace(R.id.map, supportMapFragment).commit();
+            }
+        }
+        if (supportMapFragment != null) {
+            supportMapFragment.getMapAsync(this);
+        }
         return root;
     }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in FSTG and move the camera
-        LatLng fstg = new LatLng(31.643478, -8.021075);
-        mMap.addMarker(new MarkerOptions().position(fstg).title("FSTG"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(fstg));
+    @Override public void onMapReady(GoogleMap googleMap) {
+        if (googleMap != null) {
+            googleMap.getUiSettings().setAllGesturesEnabled(true);
+            // Add a marker
+            LatLng fstg = new LatLng(31.643478, -8.021075);
+            googleMap.addMarker(new MarkerOptions().position(fstg).title("FSTG"));
+            // Adjust camera, move it to the target
+            CameraPosition camera_position = new CameraPosition.Builder().target(fstg).zoom(15.0f).build();
+            CameraUpdate camera_update = CameraUpdateFactory.newCameraPosition(camera_position);
+            googleMap.moveCamera(camera_update);
+        }
     }
-
 }
