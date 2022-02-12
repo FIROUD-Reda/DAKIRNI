55fe282141c1547b149ca135aa2f05513b826b71
kaoutar-ou <kaoutar.oubenaddi@gmail.com>
2022-02-12 21:46:40 +0100

    [OUBENADDI KAOUTAR] Contacts

--- app/src/main/java/com/example/dakirni/AdapterContact/AdapterContact.java
+++ app/src/main/java/com/example/dakirni/AdapterContact/AdapterContact.java
@@ -2,6 +2,10 @@ package com.example.dakirni.AdapterContact;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Parcelable;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +16,35 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dakirni.AddContactActivity;
 import com.example.dakirni.ParentsDashBoard;
 import com.example.dakirni.R;
+import com.example.dakirni.RetrofitInterface;
+import com.google.gson.Gson;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class AdapterContact extends RecyclerView.Adapter<AdapterContact.ItemViewHolder> {
 
-    private List<ModelClassforContact> contactList;
+    private List<Contact> contactList;
     private Context mContext;
 
-    public AdapterContact(List<ModelClassforContact>contactList, Context context) {
-        this.contactList=contactList;
+
+
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "http://192.168.8.110:5000";
+
+
+
+    public AdapterContact(List<Contact> contactList, Context context) {
+        this.contactList = contactList;
         this.mContext = context;
     }
 
@@ -31,7 +52,7 @@ public class AdapterContact extends RecyclerView.Adapter<AdapterContact.ItemView
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_design_for_contact,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_design_for_contact, parent, false);
         return new ItemViewHolder(view);
 
 
@@ -40,13 +61,12 @@ public class AdapterContact extends RecyclerView.Adapter<AdapterContact.ItemView
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
 
-        int resource = contactList.get(position).getImageview();
-        String name=contactList.get(position).getTextview1();
-        String msg=contactList.get(position).getTextview2();
-        String line=contactList.get(position).getDivider();
+        String resource = contactList.get(position).getImageview();
+        String name = contactList.get(position).getTextview1();
+        String msg = contactList.get(position).getTextview2();
+        String line = contactList.get(position).getDivider();
 
-        holder.setData(resource,name,msg,line);
-
+        holder.setData(resource, name, msg, line);
 
 
     }
@@ -59,47 +79,104 @@ public class AdapterContact extends RecyclerView.Adapter<AdapterContact.ItemView
     //view holder class
 
 
-
     public class ItemViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
         private TextView textView;
         private TextView textView2;
         private TextView divider;
+        //
+        private ImageView update_contact;
+        private ImageView delete_contact;
 
 
         public ItemViewHolder(@NonNull View itemView) {
             super(itemView);
             //here use xml ids
             //give different name not like constructor
-            imageView=itemView.findViewById(R.id.image_father);
-            textView=itemView.findViewById(R.id.name_father);
-            textView2=itemView.findViewById(R.id.Key);
-            divider=itemView.findViewById(R.id.Divider);
-            itemView.setOnClickListener(new View.OnClickListener() {
+            imageView = itemView.findViewById(R.id.image_father);
+            textView = itemView.findViewById(R.id.name_father);
+            textView2 = itemView.findViewById(R.id.Key);
+            //
+            update_contact = itemView.findViewById(R.id.update_contact);
+            delete_contact = itemView.findViewById(R.id.delete_contact);
+
+            divider = itemView.findViewById(R.id.Divider);
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    // Log.d("RecyclerView", "onClick：");
+//                    int position = getAdapterPosition();
+//                    Contact item = contactList.get(position);
+//                    ///to get the key
+//                    Toast.makeText(mContext, item.getTextview2(), Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent(mContext, ParentsDashBoard.class);
+//                    mContext.startActivity(intent);
+//                }
+//            });
+
+            // To update a contact
+            update_contact.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) {
-                    // Log.d("RecyclerView", "onClick：");
-                    int position = getAdapterPosition();
-                    ModelClassforContact item = contactList.get(position);
-                ///to get the key
-                   Toast.makeText(mContext,item.getTextview2(),Toast.LENGTH_SHORT).show();
-                    Intent intent=new Intent(mContext, ParentsDashBoard.class);
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    Contact contact = contactList.get(pos);
+//                    Toast.makeText(mContext, "pos : "+ pos + " --- " + contact.getContactId(), Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(mContext, AddContactActivity.class);
+                    intent.putExtra("update", true);
+
+                    Gson gson = new Gson();
+                    String updatedContactJson = gson.toJson(contact);
+
+                    intent.putExtra("contact", updatedContactJson);
                     mContext.startActivity(intent);
                 }
             });
 
-
+            delete_contact.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+                    retrofitInterface = retrofit.create(RetrofitInterface.class);
+                    int pos = getAdapterPosition();
+                    Contact deletedContact = contactList.get(pos);
+                    deleteContact(deletedContact);
+                }
+            });
         }
 
-        public void setData(int resource, String name, String msg,String line) {
+        public void setData(String resource, String name, String msg, String line) {
 
-           imageView.setImageResource(resource);
+
+           imageView.setImageBitmap(decodeImage(resource));
             textView.setText(name);
-           textView2.setText(msg);
+            textView2.setText(msg);
             divider.setText(line);
 
-
-
         }
+
+    }
+
+    public void deleteContact(Contact deletedContact) {
+
+        Call<Void> call = retrofitInterface.deleteContact(deletedContact);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Toast.makeText(mContext, "Contact deleted", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(mContext, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public Bitmap decodeImage(String stringImage) {
+        byte[] bytes = Base64.decode(stringImage, Base64.DEFAULT);
+        // Initialize bitmap
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        // set bitmap on imageView
+        return bitmap;
     }
 }
--- app/src/main/java/com/example/dakirni/AddContactActivity.java
+++ app/src/main/java/com/example/dakirni/AddContactActivity.java
@@ -13,13 +13,27 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.dakirni.AdapterContact.Contact;
+import com.example.dakirni.ui.contacts.ContactsFragment;
+import com.example.dakirni.ui.message.MessageFragment;
+import com.google.gson.Gson;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AddContactActivity extends AppCompatActivity {
 
@@ -28,9 +42,29 @@ public class AddContactActivity extends AppCompatActivity {
     private Button btn_add_contact_image;
     private EditText input_contact_name;
     private EditText input_contact_number;
+    private EditText editText_contact_encoded_image;
+    TextView tv_add_contact_image;
+    ImageView imageView_add_contact;
+    Bitmap bitmap;
+
+
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "http://192.168.8.110:5000";
+
+    String stringImage;
+    Contact newContact = new Contact();
+    Contact updatedContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
         btn_confirm_add_contact = findViewById(R.id.btn_confirm_add_contact);
@@ -39,6 +73,8 @@ public class AddContactActivity extends AppCompatActivity {
         input_contact_name = findViewById(R.id.input_contact_name);
         input_contact_number = findViewById(R.id.input_contact_number);
         btn_confirm_add_contact = findViewById(R.id.btn_confirm_add_contact);
+        editText_contact_encoded_image = findViewById(R.id.editText_contact_encoded_image);
+        imageView_add_contact = (ImageView) findViewById(R.id.imageView_add_contact);
 
         // add an event for the add image button
         btn_add_contact_image.setOnClickListener(new View.OnClickListener() {
@@ -47,10 +83,47 @@ public class AddContactActivity extends AppCompatActivity {
                 selectImage();
             }
         });
+
+        Bundle bundle = getIntent().getExtras();
+
+        // Add new contact
+        if (!bundle.getBoolean("update")) {
+            // event for the confirm button, to add a new contact
+            btn_confirm_add_contact.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    addNewContact();
+                }
+            });
+        }
+
+        // Update existing contact
+        if (bundle.getBoolean("update")) {
+            if (bundle.getSerializable("contact") != null) {
+                Gson gson = new Gson();
+                updatedContact = gson.fromJson(getIntent().getStringExtra("contact"), Contact.class);
+//                updatedContact = (Contact) bundle.getSerializable("contact");
+                input_contact_name.setText(updatedContact.getTextview1());
+                input_contact_number.setText(updatedContact.getTextview2());
+                stringImage = updatedContact.getImageview();
+                editText_contact_encoded_image.setText(stringImage);
+                Bitmap bitmap = decodeImage(stringImage);
+                imageView_add_contact.setImageBitmap(bitmap);
+
+                btn_confirm_add_contact.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        updateContact();
+                    }
+                });
+            }
+        }
+
     }
 
     // implicit intent to select an image
     static final int REQUEST_IMAGE_OPEN = 1;
+
     public void selectImage() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.setType("image/*");
@@ -65,17 +138,99 @@ public class AddContactActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_OPEN && resultCode == RESULT_OK) {
             Uri fullPhotoUri = data.getData();
             String str = fullPhotoUri.getPath().toString();
-            TextView tv_add_contact_image;
             tv_add_contact_image = findViewById(R.id.tv_add_contact_image);
             tv_add_contact_image.setText(str);
-            ImageView imageView_add_contact;
-            imageView_add_contact = (ImageView) findViewById(R.id.imageView_add_contact);
+//            imageView_add_contact = (ImageView) findViewById(R.id.imageView_add_contact);
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), fullPhotoUri);
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), fullPhotoUri);
                 imageView_add_contact.setImageBitmap(bitmap);
+                stringImage = encodeImage(bitmap);
+                editText_contact_encoded_image.setText(stringImage);
+//                Toast.makeText(getApplicationContext(), stringImage, Toast.LENGTH_LONG).show();
             } catch (IOException e) {
                 e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_LONG).show();
             }
+        } else {
+            Toast.makeText(getApplicationContext(), "No Image have been selected", Toast.LENGTH_LONG).show();
         }
     }
+
+    private void addNewContact() {
+        String name = input_contact_name.getText().toString();
+        String number = input_contact_number.getText().toString();
+        String image = stringImage;
+        newContact.setTextview1(name);
+        newContact.setTextview2(number);
+        newContact.setImageview(image);
+
+
+        Call<Void> call = retrofitInterface.addContact(newContact);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                input_contact_name.setText("");
+                input_contact_number.setText("");
+                stringImage = "";
+                editText_contact_encoded_image.setText("No image had been selected");
+                imageView_add_contact.setImageBitmap(null);
+                finish();
+//                Intent intent = new Intent(getApplicationContext(), ContactsFragment.class);
+//                startActivity(intent);
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void updateContact() {
+        String name = input_contact_name.getText().toString();
+        String number = input_contact_number.getText().toString();
+        String image = stringImage;
+        updatedContact.setTextview1(name);
+        updatedContact.setTextview2(number);
+        updatedContact.setImageview(image);
+
+        Call<Void> call = retrofitInterface.updateContact(updatedContact);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                input_contact_name.setText("");
+                input_contact_number.setText("");
+                stringImage = "";
+                imageView_add_contact.setImageBitmap(null);
+                finish();
+//                Intent intent = new Intent(getApplicationContext(), ContactsFragment.class);
+//                startActivity(intent);
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private String encodeImage(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        // compress Bitmap
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 40, stream);
+        // Initialize byte array
+        byte[] bytes = stream.toByteArray();
+        // get base64 encoded string
+        String encoded = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return encoded;
+    }
+
+    private Bitmap decodeImage(String stringImage) {
+        byte[] bytes = Base64.decode(stringImage, Base64.DEFAULT);
+        // Initialize bitmap
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        // set bitmap on imageView
+        return bitmap;
+    }
+
 }
